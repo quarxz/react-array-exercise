be28553f369d469039e3e8fc58609995759658ad
Falk König <fk@falkkoenig.de>
2024-01-26 15:33:22 +0100

    add filter and sort

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,40 @@ import { users } from "./data/users";
 
 function App() {
   const [btnHiglight, setBtnHighLight] = useState("");
+  const [filter, setFilter] = useState("all");
+  const [sort, setSort] = useState("");
 
-  const buttons = [
-    { id: 1, name: "All" },
-    { id: 2, name: "Women" },
-    { id: 3, name: "Men" },
-    { id: 4, name: "ByName" },
-    { id: 5, name: "ByAge" },
+  const buttonsFilter = [
+    { id: 1, name: "all" },
+    { id: 2, name: "women" },
+    { id: 3, name: "men" },
   ];
 
+  const buttonsSort = [
+    { id: 4, name: "name" },
+    { id: 5, name: "age" },
+  ];
+
+  const arr = [];
   function handleFilterAndSort(event) {
     const id = event.target.id;
     setBtnHighLight(id);
-
-    if (id == "btnAll") {
-      console.log("all", users);
+    // Fliter
+    if (id === "all") {
+      setFilter((prevfilter) => (prevfilter = "all"));
+    }
+    if (id === "women") {
+      setFilter((prevfilter) => (prevfilter = "women"));
+    }
+    if (id === "men") {
+      setFilter((prevfilter) => (prevfilter = "men"));
+    }
+    // Sort
+    if (id === "name") {
+      setSort((precSort) => (precSort = "name"));
+    }
+    if (id === "age") {
+      setSort((precSort) => (precSort = "age"));
     }
   }
 
@@ -30,25 +49,57 @@ function App() {
       </header>
       <main>
         <div className="btn-container">
-          {buttons.map((btn) => (
+          {buttonsFilter.map((btn) => (
             <button
-              id={"btn" + btn.name}
+              id={btn.name}
               onClick={handleFilterAndSort}
               className={`action-button ${
-                btnHiglight === "btn" + btn.name
-                  ? "action-button--highlight"
-                  : ""
+                filter === btn.name ? "action-button--highlight" : ""
               }`}
               key={btn.id}
             >
-              {btn.name}
+              {btn.name.charAt(0).toUpperCase() + btn.name.slice(1)}
+            </button>
+          ))}
+          {buttonsSort.map((btn) => (
+            <button
+              id={btn.name}
+              onClick={handleFilterAndSort}
+              className={`action-button ${
+                sort === btn.name ? "action-button--highlight" : ""
+              }`}
+              key={btn.id}
+            >
+              {"By " + btn.name.charAt(0).toUpperCase() + btn.name.slice(1)}
             </button>
           ))}
         </div>
         <section className="card-list">
-          {users.map((user) => {
-            return <Card key={user.email} user={user} />;
-          })}
+          {users
+            .filter((user) => {
+              if (filter === "all") {
+                return true;
+              }
+              if (filter === "women" && user.gender === "female") {
+                return true;
+              }
+              if (filter === "men" && user.gender === "male") {
+                return true;
+              }
+              return false;
+            })
+            .slice()
+            .sort((a, b) => {
+              if (sort === "age") {
+                return a.dob.age - b.dob.age;
+              }
+              if (sort === "name") {
+                return a.name.last.localeCompare(b.name.last);
+              }
+            })
+            .map((user) => {
+              return <Card key={user.email} user={user} />;
+            })}
         </section>
       </main>
     </>
