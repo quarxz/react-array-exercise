d0c595c16d56a2d6619b36bcd97e9e17835fd239
Falk König <fk@falkkoenig.de>
2024-01-26 09:27:56 +0100

    add button list

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ function App() {
         <h1>Array function magic</h1>
       </header>
       <main>
+        <div className="btn-container">
+          <button>All</button>
+          <button>Women</button>
+          <button>Men</button>
+          <button>By name</button>
+          <button>By age</button>
+        </div>
         <section className="card-list">
           {users.map((user) => {
             return <Card key={user.email} user={user} />;
